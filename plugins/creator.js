ace10b3d1812189510ed30f265986456f0af4d0f
RimuruBotMd <108895247+RimuruBotMd@users.noreply.github.com>
2022-09-09 13:13:59 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -21,16 +21,16 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*💌 Nama* : Fangz
-*✉️ Nama RL* : FangzBOT
-*♂️ Gender* : Laki - laki
+*💌 Nama* : Kanna
+*✉️ Nama RL* : Kanna Chan
+*♂️ Gender* : Prempuan
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 25 oktober 2007
+*⏰ Tanggal lahir* : 10 Aprill 2008
 *🎨 Umur* : 15
 *🧮 Kelas* : 9
 *🧩 Hobby* : Nonton henta*, main game, Recode script
 *💬 Sifat* : Baik, hode, softboy, tydack ramah, g*y
-*🗺️ Tinggal* : Indonesia, jawa, bogor
+*🗺️ Tinggal* : Indonesia, jawa, Tegal
 *❤️ Suka* : warnah pink & hitam, trap, waifu wangy, kucing
 *💔 Benci* : anak alay, autis, anak epep, sok asik
 
@@ -48,8 +48,8 @@ let teksbio = `${htki} *BIODATA* ${htka}
 🐈 *Github:* ${sgh}
 🥏 *Whatsapp* wa.me/${nomorown}
 🌐 *Discord:* Soon
-🌏 *Website:* https://fangzbot.websites.co.in/
-🎶 *Tiktok:* tiktok.com/@fangzjb
+🌏 *Website:* https://kannachan.websites.co.in/
+🎶 *Tiktok:* tiktok.com/@kannachan
 
 `
   let teks = ' '
